b3f1b429470988f48e7e9ccd45f2461430c29007
matiescobar2023 <127998716+matiescobar2023@users.noreply.github.com>
2024-04-04 12:02:43 -0300

    Add files via upload

---
+++ Proyecto/Simulacion_PMSM/parametros.m
@@ -0,0 +1,17 @@
+clear all
+Jm = 3.1e-6;
+bm = 1.5e-5;
+Pp = 3;
+rs = 1.02;
+Ld = 6.6/1000;
+Lq = 5.8/1000;
+lam = 0.01546;
+Lls = 0.8/1000;
+Kl = 1;
+
+%Variables controlador
+Rqp = 1;
+Rdp = 1;
+Ksiq = 1;
+Ksa = 1;
+ba = 1;
