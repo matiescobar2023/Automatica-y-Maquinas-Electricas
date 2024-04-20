853146204166c83b15cc9c831dd207db3d784ea6
matiescobar2023 <127998716+matiescobar2023@users.noreply.github.com>
2024-04-19 21:29:49 -0300

    Add files via upload

--- Proyecto/Simulacion_PMSM/parametros.m
+++ Proyecto/Simulacion_PMSM/parametros.m
@@ -8,10 +8,19 @@ Lq = 5.8/1000;
 lam = 0.01546;
 Lls = 0.8/1000;
 Kl = 1;
-
+Jl = 1;
+r = 120;
+aa = 1;
+Tref = 25;
+Rsref = 10;
+Rts = 10;
+Cts = 10;
+bl = 1;
+Jeq = Jm+Jl/(r^2);
+beq = bm+bl/(r^2);
 %Variables controlador
-Rqp = 1;
-Rdp = 1;
+Rqp = 2;
+Rdp = 3;
 Ksiq = 1;
 Ksa = 1;
 ba = 1;
