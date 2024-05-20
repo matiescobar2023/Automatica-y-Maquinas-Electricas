9f60a350e3168976dd32114858a1c0c05769412c
matiescobar2023 <meescobarf87@gmail.com>
2024-05-20 16:01:25 -0300

    Agrego simulacion 5_1_6_a

--- Proyecto/Simulacion_PMSM/parametros.m
+++ Proyecto/Simulacion_PMSM/parametros.m
@@ -7,6 +7,7 @@ r = 120;
 J_m = 1.4e-5;
 b_m = 1.5e-5;
 P_p = 3;
+Pp = 3;
 lambda_m = 0.01546;
 L_q = 5.8/1000;
 L_d = 6.6/1000;
@@ -16,6 +17,7 @@ C_ts = 0.818;
 R_ts_amb = 146.7;
 T_sREF = 40;
 R_sREF = 1.02;
+R_s = 1.02;
 T_amb = 25;
 
 J_eq = J_m + J_l/(r^2);
@@ -27,17 +29,17 @@ Ksiq = 1;
 Ksa = 1;
 ba = 1;
 
-syms dtheta_m(t) omega_m(t) J_eq domega_m(t) T_m(t) b_eq T_l(t) v_{qs}^r(t) R_s(t) i_{qs}^r(t) L_q di_{qs}^r(t) lambda_{m}^r L d_i_dsr(t) omega_r(t) v_{ds}^r(t) i_{ds}^r(t) id_s(t) Lls di0_s(t) T_perd(t) Cts dT0_s(t) Rts_amb Tamb(t) i_0s(t) i_2as(t) i_2bs(t) i_2cs(t) i_2qs(t) i_2ds(t) i_20s(t) Pp Rs_REF alpha_Cu Ts_REF;
+%syms dtheta_m(t) omega_m(t) J_eq domega_m(t) T_m(t) b_eq T_l(t) v_{qs}^r(t) R_s(t) i_{qs}^r(t) L_q di_{qs}^r(t) lambda_{m}^r L d_i_dsr(t) omega_r(t) v_{ds}^r(t) i_{ds}^r(t) id_s(t) Lls di0_s(t) T_perd(t) Cts dT0_s(t) Rts_amb Tamb(t) i_0s(t) i_2as(t) i_2bs(t) i_2cs(t) i_2qs(t) i_2ds(t) i_20s(t) Pp Rs_REF alpha_Cu Ts_REF;
 
 % Ecuaciones
-eqn1 = dtheta_m(t) == omega_m(t);
-eqn2 = J_eq * diff(omega_m(t), t) == T_m(t) - b_eq * omega_m(t) - T_l(t);
-eqn3 = v_qsr(t) == Rs(t) * i_qsr(t) + Lq * diff(i_qsr(t), t) + (lambda_m_r + L * diff(i_dsr(t), t)) * omega_r(t);
-eqn4 = v_dsr(t) == Rs(t) * i_dsr(t) + L * diff(i_dsr(t), t) - Lq * i_qsr(t) * omega_r(t);
-eqn5 = v_0s(t) == Rs(t) * id_s(t) + Lls * diff(i0_s(t), t);
-eqn6 = T_m(t) == (3/2) * Pp * (lambda_m_r * i_qsr(t) + (L - Lq) * i_dsr(t) * i_qsr(t));
-eqn7 = Rs(t) == Rs_REF * (1 + alpha_Cu * (Ts(t) - Ts_REF));
-eqn8 = Ps_perd(t) == Cts * diff(Ts(t), t) + (1/Rts_amb) * (Ts(t) - Tamb(t));
-eqn9 = Ps_perd(t) == Rs(t) * (i_2as(t) + i_2bs(t) + i_2cs(t)) == (3/2) * Rs(t) * (i_2qs(t) + i_2ds(t) + 2 * i_20s(t));
-
+% eqn1 = dtheta_m(t) == omega_m(t);
+% eqn2 = J_eq * diff(omega_m(t), t) == T_m(t) - b_eq * omega_m(t) - T_l(t);
+% eqn3 = v_qsr(t) == Rs(t) * i_qsr(t) + Lq * diff(i_qsr(t), t) + (lambda_m_r + L * diff(i_dsr(t), t)) * omega_r(t);
+% eqn4 = v_dsr(t) == Rs(t) * i_dsr(t) + L * diff(i_dsr(t), t) - Lq * i_qsr(t) * omega_r(t);
+% eqn5 = v_0s(t) == Rs(t) * id_s(t) + Lls * diff(i0_s(t), t);
+% eqn6 = T_m(t) == (3/2) * Pp * (lambda_m_r * i_qsr(t) + (L - Lq) * i_dsr(t) * i_qsr(t));
+% eqn7 = Rs(t) == Rs_REF * (1 + alpha_Cu * (Ts(t) - Ts_REF));
+% eqn8 = Ps_perd(t) == Cts * diff(Ts(t), t) + (1/Rts_amb) * (Ts(t) - Tamb(t));
+% eqn9 = Ps_perd(t) == Rs(t) * (i_2as(t) + i_2bs(t) + i_2cs(t)) == (3/2) * Rs(t) * (i_2qs(t) + i_2ds(t) + 2 * i_20s(t));
+% 
 
