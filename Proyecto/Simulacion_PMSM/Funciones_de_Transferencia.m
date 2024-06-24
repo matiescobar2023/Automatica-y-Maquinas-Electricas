524368a56cbc29953ab45e9f2cfdbfe9627aa9bd
Juan <borquez.juan00@gmail.com>
2024-06-23 21:58:55 -0300

    actualización

--- Proyecto/Simulacion_PMSM/Funciones_de_Transferencia.m
+++ Proyecto/Simulacion_PMSM/Funciones_de_Transferencia.m
@@ -1,5 +1,25 @@
-parametros
-syms s %Theta_m Omega_m I_qs T_l V_qs J_eq b_eq P_p lambda_m r L_q R_s
+%parametros
+clc; clear;
+syms s Theta_m Omega_m I_qs T_l V_qs J_eq b_eq P_p lambda_m r L_q R_s
+A = [ 0         1                           0
+      0     -b_eq/J_eq         3*P_p*lambda_m/(2*J_eq)
+      0  -P_p*lambda_m/L_q             -R_s/L_q      ];
+[P, D] = eig(A);
+
+syms Z1 Z2
+z1 = D(2,2);
+z2 = D(3,3);
+% P = subs(P, z1, Z1);
+% P = subs(P, z2, Z2);
+
+omega_n = ((R_s*b_eq + (3/2)*P_p^2*lambda_m^2)/(J_eq*L_q))^(1/2);
+zita    = (J_eq*R_s + L_q*b_eq)/(2*(J_eq*L_q*(R_s*b_eq + (3/2)*P_p^2*lambda_m^2))^(1/2));
+zero    = -R_s/L_q;
+
+syms OMEGA_N ZITA
+P = subs(P, omega_n, OMEGA_N);
+P = subs(P, zita, ZITA);
+
 
 % eq1 = s*Theta_m == Omega_m
 % 
@@ -16,7 +36,7 @@ syms s %Theta_m Omega_m I_qs T_l V_qs J_eq b_eq P_p lambda_m r L_q R_s
 % sol_2 = simplify(sol_2)*(0.5/0.5)
 % latex(sol_2)
 
-pol_c = J_eq*L_q*s^3 + (J_eq*R_s+L_q*b_eq)*s^2 + (R_s*b_eq+(3*P_p^2*lambda_m^2)/2)*s==0
-polos = solve(pol_c,s)
-polos =  simplify(polos)
-polos_l = latex(polos(3))
+% pol_c = J_eq*L_q*s^3 + (J_eq*R_s+L_q*b_eq)*s^2 + (R_s*b_eq+(3*P_p^2*lambda_m^2)/2)*s==0
+% polos = solve(pol_c,s)
+% polos =  simplify(polos)
+% polos_l = latex(polos(3))
